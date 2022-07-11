f563ef8315797854c43dcc2f0481069123906117
SilentCoder2005 <109058436+SilentCoder2005@users.noreply.github.com>
2022-07-11 17:27:26 +0530

    Working

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/NoobEditorCodes/TmIMDBElsa.git /TmIMDBElsa
+  git clone https://github.com/SilentCoder2005/Techno_Mindz.git /Techno_Mindz
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TmIMDBElsa
+  git clone $UPSTREAM_REPO /Techno_Mindz
 fi
-cd /TmIMDBElsa
+cd /Techno_Mindz
 pip3 install -U -r requirements.txt
-echo "Starting Elsa ❄️❄️"
+echo "Starting Elsa ❄️❄️ 5,4,3,2,1..."
 python3 bot.py
